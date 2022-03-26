a92f1c46ad0ff0b75232eacc5340c4ab5510d334
Paavo Pere <paavo@pere.fi>
2022-03-26 23:10:16 +0200

    more TDD and some wiktionary lookups

--- palabras/core.py
+++ palabras/core.py
@@ -1,13 +1,31 @@
+from dataclasses import dataclass, field
+from typing import List
 import requests
 import bs4
 from bs4 import BeautifulSoup
+from bs4.element import PageElement
 
 
 WiktionaryPage = str
 WiktionaryPageSection = str
 
-def lookup(word: str):
-    return word
+
+class WiktionaryPageNotFound(LookupError):
+    pass
+
+
+class WiktionarySectionNotFound(LookupError):
+    pass
+
+
+@dataclass
+class WordInfo:
+    word: str
+    definition: list = field(default_factory=list)
+
+
+def lookup(word: str) -> WordInfo:
+    return WordInfo(word)
 
 
 def get_wiktionary_spanish_section(word: str) -> WiktionaryPageSection:
@@ -16,18 +34,28 @@ def get_wiktionary_spanish_section(word: str) -> WiktionaryPageSection:
 
 def get_wiktionary_page(word: str) -> WiktionaryPage:
     r = requests.get(f'https://en.wiktionary.org/wiki/{word}')
+    if 'Wiktionary does not yet have an entry for' in r.text:
+        raise WiktionaryPageNotFound('Wiktionary page not found for {word}')
     return r.text
 
 
 def extract_spanish_section(page: WiktionaryPage) -> WiktionaryPageSection:
+    soup = BeautifulSoup(page, features='html.parser')
+    section_id_element = soup.find(id='Spanish')
+    if section_id_element is None:
+        raise WiktionarySectionNotFound()
+    start_element = section_id_element.parent
+    assert start_element.name == 'h2'
+    elements = _get_siblings_until_h1_or_h2(start_element)
+    return '\n'.join(map(str, elements))
     
-    soup = BeautifulSoup(page)
-    start_tag = soup.find(id='Spanish').parent
-    section_tags = [start_tag]
-    for sibling in start_tag.next_siblings:
-        if sibling.name == start_tag.name:
+
+def _get_siblings_until_h1_or_h2(element: PageElement) -> List[PageElement]:
+    section_tags = [element]
+    for sibling in element.next_siblings:
+        if sibling.name in ('h1', 'h2'):
             break
         else:
             section_tags.append(sibling)
     
-    raise NotImplementedError
+    return section_tags
--- test/test_palabras.py
+++ test/test_palabras.py
@@ -1,17 +1,24 @@
 
+import pytest
 import palabras.core
 
 
 def test_lookup_returns_str():
     word = 'despacito'
     result = palabras.core.lookup(word)
-    assert isinstance(result, str)
+    assert isinstance(result, palabras.core.WordInfo)
 
 
 def test_get_wiktionary_page_returns_str():
     word = 'despacito'
     result = palabras.core.get_wiktionary_page(word)
     assert isinstance(result, str)
+
+
+def test_get_wiktionary_page_nonexistent():
+    word = 'thispageaintexistent'
+    with pytest.raises(palabras.core.WiktionaryPageNotFound):
+        palabras.core.get_wiktionary_page(word)
 
 
 def test_get_wiktionary_page_contains_translation():
@@ -34,9 +41,21 @@ def test_get_wiktionary_spanish_section():
     assert isinstance(result, str)
 
 
+def test_no_spanish_definition():
+    word = 'kauppa'  # a word that has Wiktionary page but no Spanish definition
+    with pytest.raises(palabras.core.WiktionarySectionNotFound):
+        palabras.core.get_wiktionary_spanish_section(word)
+
+
 def test_get_wiktionary_spanish_section_does_not_contain_portuguese():
     word = 'culpar'
     portuguese_conjugation = 'culpou'  # Portuguese 3rd person preterite
     section = palabras.core.get_wiktionary_spanish_section(word)
     assert portuguese_conjugation not in section
-    
+
+
+@pytest.mark.xfail
+def test_lookup_definition():
+    word = 'culpar'
+    word_info = palabras.core.lookup(word)
+    assert word_info.definition[0] == 'to blame'
