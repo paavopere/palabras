2882a958c87ca20c3fc8cff271ce07fe732244f2
Paavo Pere <paavo@pere.fi>
2023-07-02 02:15:22 +0300

    deploy api gateway

--- deploy/main.tf
+++ deploy/main.tf
@@ -107,7 +107,8 @@ resource "aws_iam_role_policy_attachment" "palabras" {
   role = aws_iam_role.palabras.name
 }
 
-# lambda function
+# zip the code and deploy to lambda function
+
 data "archive_file" "python_lambda_package" {
   type        = "zip"
   source_dir  = "${path.module}/.."
@@ -139,4 +140,82 @@ resource "aws_lambda_function" "palabras" {
     subnet_ids         = [aws_subnet.palabras.id]
     security_group_ids = [aws_security_group.palabras.id]
   }
+}
+
+# api gateway
+
+resource "aws_apigatewayv2_api" "palabras" {
+  name = "palabras"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "palabras" {
+  api_id = aws_apigatewayv2_api.palabras.id
+  name = "palabras"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    })
+  }
+
+  route_settings {
+    route_key = "GET /"
+    logging_level = "INFO"
+    throttling_burst_limit = 1
+    throttling_rate_limit = 1
+  }
+}
+
+resource "aws_apigatewayv2_integration" "palabras" {
+  api_id = aws_apigatewayv2_api.palabras.id
+
+  integration_uri    = aws_lambda_function.palabras.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "palabras" {
+  api_id = aws_apigatewayv2_api.palabras.id
+
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.palabras.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.palabras.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.palabras.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.palabras.execution_arn}/*/*"
+}
+
+
+output "function_name" {
+  description = "Name of the Lambda function."
+  value = aws_lambda_function.palabras.function_name
+}
+
+output "base_url" {
+  description = "Base URL for API Gateway stage."
+  value = aws_apigatewayv2_stage.palabras.invoke_url
 }
